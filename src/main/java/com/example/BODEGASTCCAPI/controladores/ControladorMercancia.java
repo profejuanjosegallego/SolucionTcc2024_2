9cc8a3350b1ab26bdb7344c91caa4c379eddfb51
jjosegallegocesde <jjosegallegomesa@gmail.com>
2024-10-08 07:24:42 -0500

    se crea documentacion

--- src/main/java/com/example/BODEGASTCCAPI/controladores/ControladorMercancia.java
+++ src/main/java/com/example/BODEGASTCCAPI/controladores/ControladorMercancia.java
@@ -1,7 +1,16 @@
 package com.example.BODEGASTCCAPI.controladores;
 
 import com.example.BODEGASTCCAPI.modelos.Mercancia;
+import com.example.BODEGASTCCAPI.modelos.dto.MercanciaDTO;
 import com.example.BODEGASTCCAPI.servicios.MercanciaServicio;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +24,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/soluciontcc/v1/mercancias")
+@Tag(name="Servicios asociados a la entidad o tabla mercancias", description = "\nse hace CRUD completo a la tabla mercancia permitiendo lectura y escritura de datos")
 public class ControladorMercancia {
 
     //Inyectar una dependencia al servicio
@@ -24,13 +34,37 @@ public class ControladorMercancia {
     //llamar a cada uno de los metodos disponibles
     // en el servicio
 
-
-
     @PostMapping()
+    @Operation(
+            summary = "Registra una mercancia nueva en la base de datos",
+            description = "al llevar los datos del modelo mercancia se permite un registro exitoso del objeto en BD"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Mercancia almecenada con exito en BD",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = MercanciaDTO.class),
+                                    examples = @ExampleObject(value = "{\"volumen\":\"20.5\",\"peso\":\"400\",\"nombre\":\"Nevera LG\",\"direccion\":\"calle 100 sur 123\",\"fechaIngreso\":\"2024-10-8\"}")
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Error al registrar la mercancia",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class),
+                                    examples = @ExampleObject(value = "{\"mensaje\":\"eL volumen no puede ser negativo\"}")
+                            )
+                    )
+            }
+    )
     public ResponseEntity<?> LlamadoGuardarMercanciaDTO(@RequestBody Mercancia datosMercanciaEnviadosCliente){
         try{
             return ResponseEntity
-                    .status(HttpStatus.OK)
+                    .status(HttpStatus.CREATED)
                     .body(this.mercanciaServicio.almacenarMercanciaDTO(datosMercanciaEnviadosCliente));
         }catch(Exception error){
             HashMap<String, Object> mensajeRespuesta= new HashMap<>();
